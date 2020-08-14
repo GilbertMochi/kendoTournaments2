539d556b3faf3056b160b21c196b6635b05e1138
Janessa <s00190968@mail.itsligo.ie>
2020-08-14 17:21:07 +0300

    added firebase to environment.prod and built the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBxa622hwqenFk9B_Zfu4qn5c8D-C2HV04",
+    authDomain: "fir-testwithangular1.firebaseapp.com",
+    databaseURL: "https://fir-testwithangular1.firebaseio.com",
+    projectId: "fir-testwithangular1",
+    storageBucket: "fir-testwithangular1.appspot.com",
+    messagingSenderId: "426797734019",
+    appId: "1:426797734019:web:0d4d86aeb2ac035ba4e3b6",
+    measurementId: "G-1JKHMRMTLL"
+  }
 };
